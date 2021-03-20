00235eb41d46a7808a75da102b41a33ee9bb3067
Iliya <iliyamiroshnichenko88@gmail.com>
2021-03-20 21:41:20 +0200

    add Statistics component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import Profile from "./components/Profile/Profile";
-import user from "./user.json";
+import Statistics from "./components/Statistics/Statistics";
+import FriendList from "./components/FriendList/FriendList";
+import user from "./json/user.json";
+import statisticalData from "./json/statistical-data.json";
+import friends from "./json/friends.json";
 
 function App() {
   return (
@@ -11,6 +15,8 @@ function App() {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={statisticalData} />
+      {/* <Statistics stats={statisticalData} /> */}
     </>
   );
 }
