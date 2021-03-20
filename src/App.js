a57b204c68d80b65bb845991a18940ab53fa1d1a
Iliya <iliyamiroshnichenko88@gmail.com>
2021-03-20 20:52:38 +0200

    add Profile component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Profile from "./components/Profile/Profile";
+import user from "./user.json";
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </>
+  );
 }
 
 export default App;
