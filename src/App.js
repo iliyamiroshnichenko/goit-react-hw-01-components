1a40dc225bc06048ba13d6f155889d5f2e9d2404
Iliya <iliyamiroshnichenko88@gmail.com>
2021-03-23 22:43:15 +0200

    add styles to Profile & Statistics

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import Profile from "./components/Profile/Profile";
-import Statistics from "./components/Statistics/Statistics";
-import FriendList from "./components/FriendList/FriendList";
-import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
-import user from "./json/user.json";
-import statisticalData from "./json/statistical-data.json";
-import friends from "./json/friends.json";
-import transactions from "./json/transactions.json";
+import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
+import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import user from './json/user.json';
+import statisticalData from './json/statistical-data.json';
+import friends from './json/friends.json';
+import transactions from './json/transactions.json';
+import Container from './components/Container/Container';
 
 function App() {
   return (
-    <>
+    <Container>
       <Profile
         name={user.name}
         tag={user.tag}
@@ -21,7 +22,7 @@ function App() {
       {/* <Statistics stats={statisticalData} /> */}
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />;
-    </>
+    </Container>
   );
 }
 
